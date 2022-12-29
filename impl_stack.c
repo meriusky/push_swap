2db8f73f2f2358637d9c31921e65d2d1442e786d
meriusky <meri.hernandez997@gmail.com>
2022-12-29 23:24:14 +0100

    stack impl file in process 2

--- impl_stack.c
+++ impl_stack.c
@@ -6,7 +6,7 @@
 /*   By: mehernan <mehernan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 14:44:55 by mehernan          #+#    #+#             */
-/*   Updated: 2022/12/29 23:02:41 by mehernan         ###   ########.fr       */
+/*   Updated: 2022/12/29 23:23:28 by mehernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,26 @@ typedef struct s_elem
 	struct s_elem	*next;
 }t_elem;
 
-typedef struct stack
+typedef struct s_stack
 {
 	struct s_elem	*first;
 	int	size;
 }t_stack;
 
-int	*init_stack()
+t_stack	*init_stack()
 {
 	t_stack	*save;
 
 	save = malloc(sizeof(stack));
 	if(!save)
 		return(NULL);
-	t_stack->size=0;
+	save->size=0;
 	return(save);
 }
 
 void push(stack *s, int value) //añade un elemento a la stack
 {
-	s_elem *elem;
+	t_elem *elem;
 	
 	elem = malloc(sizeof(s_elem));
 	if(!elem)
@@ -43,12 +43,14 @@ void push(stack *s, int value) //añade un elemento a la stack
 	elem->value = value;
 	elem->next = s->first;
 	s->first = elem:
-	stack->size++;
+	s->size++;
 }
 
-void pop(stack *s)
+void pop(t_stack *s)
 {
-	s_elem* first = s.first;
+	s_elem* first // si,una estructura puede ser puntero
+
+	first = s->first;
 	s->first = first->next;
 	free(first);
 	s->size--;
